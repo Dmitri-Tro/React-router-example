62c10475170a533fe2516b3185dec229b89c8599
Dmitri-Tro <dtroshchenkov@gmail.com>
2024-10-15 19:46:08 +0300

    Index path

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorPage } from './ErrorPage';
+import { Index } from './routes';
 import { Contact, contactLoader } from './routes/Contact';
 import { destroyAction } from './routes/Destroy';
 import { editAction, EditContact } from './routes/Edit';
 import { Root, rootLoader, rootAction } from './routes/Root';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter( [
   {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
+    path: '/',
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index/> },
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
+        path: 'contacts/:contactId',
+        element: <Contact/>,
         loader: contactLoader,
       },
       {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
+        path: 'contacts/:contactId/edit',
+        element: <EditContact/>,
         loader: contactLoader,
         action: editAction,
       },
       {
-        path: "contacts/:contactId/destroy",
+        path: 'contacts/:contactId/destroy',
         action: destroyAction,
         errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
-]);
+] );
 
-createRoot(document.getElementById('root')).render(
+createRoot( document.getElementById( 'root' ) ).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={ router }/>
   </StrictMode>,
-)
+);
